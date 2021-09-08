91131ea44127cc71c4f244c067608a1cdfb50b17
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-09-08 00:39:34 -0300

    Prettier formatting

--- src/api/api.js
+++ src/api/api.js
@@ -1,49 +1,49 @@
 export const Api = {
-	baseUrl: "https://rmback.herokuapp.com",
+  baseUrl: "https://rmback.herokuapp.com",
 
-	createUrl: () => Api.baseUrl + "/characters",
+  createUrl: () => Api.baseUrl + "/characters",
 
-	readAllUrl: () => Api.baseUrl + "/characters",
-	readSingleUrl: (id) => Api.baseUrl + "/characters/" + id,
+  readAllUrl: () => Api.baseUrl + "/characters",
+  readSingleUrl: (id) => Api.baseUrl + "/characters/" + id,
 
-	updateUrl: (id) => Api.baseUrl + "/characters/" + id,
+  updateUrl: (id) => Api.baseUrl + "/characters/" + id,
 
-	deleteUrl: (id) => Api.baseUrl + "/characters/" + id,
-	deleteAllUrl: () => Api.baseUrl + "/",
+  deleteUrl: (id) => Api.baseUrl + "/characters/" + id,
+  deleteAllUrl: () => Api.baseUrl + "/",
 
-	// Create
-	buildApiPostRequest: (url, body) => {
-		return fetch(url, {
-			method: "POST",
-			headers: new Headers({
-				"Content-type": "application/json",
-			}),
-			body: JSON.stringify(body),
-		});
-	},
+  // Create
+  buildApiPostRequest: (url, body) => {
+    return fetch(url, {
+      method: "POST",
+      headers: new Headers({
+        "Content-type": "application/json",
+      }),
+      body: JSON.stringify(body),
+    });
+  },
 
-	// ReadAll
-	buildApiGetRequest: (url) => {
-		return fetch(url, {
-			method: "GET",
-		});
-	},
+  // ReadAll
+  buildApiGetRequest: (url) => {
+    return fetch(url, {
+      method: "GET",
+    });
+  },
 
-	// UpdateById
-	buildApiPutRequest: (url, body) => {
-		return fetch(url, {
-			method: "PUT",
-			headers: new Headers({
-				"Content-type": "application/json",
-			}),
-			body: JSON.stringify(body),
-		});
-	},
+  // UpdateById
+  buildApiPutRequest: (url, body) => {
+    return fetch(url, {
+      method: "PUT",
+      headers: new Headers({
+        "Content-type": "application/json",
+      }),
+      body: JSON.stringify(body),
+    });
+  },
 
-	// DeleteAll
-	buildApiDeleteRequest: (url) => {
-		return fetch(url, {
-			method: "DELETE",
-		});
-	},
+  // DeleteAll
+  buildApiDeleteRequest: (url) => {
+    return fetch(url, {
+      method: "DELETE",
+    });
+  },
 };
